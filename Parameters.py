cc040eb482c3e34807a746bb3c8d3535f6ea3eb2
Rhiannon-B <123592119+Rhiannon-B@users.noreply.github.com>
2023-02-09 09:50:54 +0000

    Create Parameters.py

---
+++ Parameters.py
@@ -0,0 +1,18 @@
+b0 = 2.5
+gamma = 0.75
+c = 1
+r = 0.25
+a = 0.125
+xi1 = 0
+xi2 = 0
+
+
+# Scaling terms
+depth_scale = 150
+temp_scale = 7.5
+time_scale = 2
+
+# Annual mu terms
+mu0 = 0.75
+mu_annual = 0.2
+tau = 12/time_scale
